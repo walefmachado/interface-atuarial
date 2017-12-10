589b0ee2d48c56fd57723192e11372f7e6844fb6
Walef Machado <walefm2@hotmail.com>
2017-12-10 19:37:07 -0200

    Escolha da tábua e do sexo

--- teste_interface_seca.R
+++ teste_interface_seca.R
@@ -15,12 +15,13 @@ ui <- dashboardPage(
   dashboardSidebar(
     
     
-    selectInput("seg", "Selecione o seguro:",choices = c("Seguro Temporário" = 1 ,"Previdência" = 2,"Anuidade" = 3),multiple = F),
+    selectInput("seg", "Selecione o seguro:",choices = c("Seguro Temporário" = 1 ,"Seguro Vitalício" = 2,"Anuidade" = 3),multiple = F),
+    selectInput("tab", "Selecione a tábua de vida", choices = c("AT 49" = 1, "AT 83" = 2, "AT 20000" = 3)),
     selectInput("sex", "Sexo:",choices = c("Masculino" = 1 ,"Feminino" = 2), multiple = F),
     numericInput("ben", "Beneficio", min = 0, max = Inf, value = 1),
     numericInput("idade", "Idade", min = 0, max = (nrow(dados)-1), value = 0, step = 1),
-    sliderInput("n", "Período",min = 0, max = (nrow(dados)-1), animate = F, value = 1, step = 1),
-    sliderInput("tx", "Taxa de juros", min = 0, max = 1, value = 0.06, step = 0.001 ),
+    numericInput("n", "Período",min = 0, max = (nrow(dados)-1), value = 1, step = 1),
+    numericInput("tx", "Taxa de juros", min = 0, max = 1, value = 0.06, step = 0.001 ),
     
     #Se a Seguro vitalício for selecionada...
     conditionalPanel(condition = "input.dist==1",numericInput("Período","Taxa de juros",value = 0),
@@ -43,11 +44,21 @@ ui <- dashboardPage(
 
 
 # Funções -----------------------------------------------------------------
+dados <- read.table('/home/walef/Dropbox/SECA Programação/tábuas_leitura_R.txt', h=T)
+attach(dados)
+Premio_A <- function( i, idade, n, b, qx){ # i = taxa de juros, n = tempo, b = valor do beneficio
+  px <- 1-qx
+  f.desconto <- 1/(i+1)
+  v <- f.desconto^(1:n)
+  qxx <- c(qx[(idade+1):(idade+n)])
+  pxx <- c(1, cumprod( px[(idade+1):(idade+n-1)]) )
+  Ax <- b* sum(v*pxx*qxx)
+  Ax <- round(Ax, 2)
+  return (Ax)
+}
 
-Premio_A <- function( i, idade, n, b, dados){ # i = taxa de juros, n = tempo, b = valor do beneficio
-  dados <- read.table('/home/walef/Dropbox/SECA Programação/tábuas_leitura_R.txt', h=T)
-  attach(dados)
-  qx <- dados$AT_2000B_M_qx
+Premio_V <- function( i, idade, b, qx){ # i = taxa de juros, n = tempo, b = valor do beneficio
+  n <- max(Idade)-25 
   px <- 1-qx
   f.desconto <- 1/(i+1)
   v <- f.desconto^(1:n)
@@ -66,19 +77,34 @@ server <- function(input, output) {
   
   output$hist <- renderPrint({
     if((max(dados$Idade)-input$idade) >= input$n){
+      if(input$tab==1){
+        qx <- dados$AT_49_qx
+      }
+      if(input$tab==2){
+        qx <- dados$AT_83_qx
+      }
+      if(input$tab==3){
+        if(input$sex==1){
+          qx <- dados$AT_2000B_M_qx
+        }
+        if(input$sex==2){
+          qx <- dados$AT_2000B_F_qx
+        }
+      }
       if(input$seg==1){
-        a <- Premio_A(input$tx, round(input$idade, 0), input$n, input$ben, input$dados)
+        a <- Premio_A(input$tx, round(input$idade, 0), input$n, input$ben, qx)
       }
       if(input$seg==2){
-        a <- Premio_A(input$tx, input$idade, input$n, input$ben)}
+        a <- Premio_V(input$tx, input$idade, input$ben, qx)}
       if(input$seg==3){
-        a <- Premio_A(input$tx, input$idade, input$n, input$ben)
+        a <- Premio_A(input$tx, input$idade, input$n, input$ben, qx)
       }
       cat('O valor do seu prêmio é:', a)
     }else{
       cat('O período temporário está errado')
-   } 
-})
+    } 
+  })
+  
   
   # output$grafico <- renderPlot({
   #   if(input$seg==1){
