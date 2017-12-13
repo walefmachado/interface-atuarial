c71125296fd9e065e0b4a50d66a1e7af15749d8c
PedroOliveira00 <pedrohenriquero@hotmail.com>
2017-12-13 16:58:37 -0200

    Alterecoes_Pedro

--- teste_interface_seca.R
+++ teste_interface_seca.R
@@ -20,6 +20,8 @@ ui <- dashboardPage(
     #Dotal Puro (Pedro)
     selectInput("seg", "Selecione o seguro:",choices = c("Seguro Temporário" = 1 ,"Seguro Vitalício" = 2,"Anuidade" = 3, "Dotal Puro" = 4, "Dodal Misto" = 5),multiple = F),
     selectInput("tab", "Selecione a tábua de vida", choices = c("AT 49" = 1, "AT 83" = 2, "AT 2000" = 3)),
+    checkboxInput("care", "Carencia", value = FALSE),
+    conditionalPanel(condition = "input.care == TRUE", numericInput("TempCar", "Tempo de Carencia", value = 1)),
     # Se a tábua at2000 for selecionada então o individuo pode escolher o sexo do participante.
     conditionalPanel(condition = "input.tab == 3", selectInput("sex", "Sexo:",choices = c("Masculino" = 1 ,"Feminino" = 2), multiple = F)),
     numericInput("ben", "Beneficio ($)", min = 0, max = Inf, value = 1),
@@ -121,20 +123,29 @@ server <- function(input, output) {
           qx <- dados$AT_2000B_F_qx
         }
       }
+      
+      if(input$care == "FALSE"){
+        care = 1
+        m = 0
+      }else{
+        care = Dotal_Puro(input$tx, input$idade,input$TempCar, 1 , qx)
+        m = input$TempCar
+      }
+      
       if(input$seg==1){
-        a <- SV_Temp(input$tx, round(input$idade, 0), input$n, input$ben, qx)
+        a <- SV_Temp(input$tx, round(input$idade, 0), input$n, input$ben, qx, care, m)
       }
       if(input$seg==2){
-        a <- SV_Vit(input$tx, input$idade, input$ben, qx)
+        a <- SV_Vit(input$tx, input$idade, input$ben, qx, care, m)
         }
       if(input$seg==3){
-        a <- Anuid(input$tx, input$idade, input$ben, qx)
+        a <- Anuid(input$tx, input$idade, input$ben, qx, care, m)
       }
       if(input$seg==4){
-        a <- Dotal_Puro(input$tx, input$idade, input$n, input$ben, qx)
+        a <- Dotal_Puro(input$tx, input$idade, input$n, input$ben, qx, care, m)
       }
       if(input$seg==5){
-        a <- Dotal(input$tx, input$idade, input$n, input$ben, qx)
+        a <- Dotal(input$tx, input$idade, input$n, input$ben, qx, care, m)
       }
       cat('O valor do seu prêmio puro único é:', a)
     }else{
