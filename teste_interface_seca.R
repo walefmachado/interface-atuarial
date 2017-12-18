c7c7304760ac668d2577f9fd9d701adcb6a4b515
Walef Machado <walefm2@hotmail.com>
2017-12-18 13:56:30 -0200

    Adicionando abas

--- teste_interface_seca.R
+++ teste_interface_seca.R
@@ -13,49 +13,44 @@ dados <- read.table(local, h=T)
 # dados <- read.table('/home/walef/Dropbox/SECA Programação/tábuas_leitura_R.txt', h=T)
 
 ui <- dashboardPage(
-  dashboardHeader(title = "Seguros"),
+  dashboardHeader(title = "Cálculo Atuarial"),
   dashboardSidebar(
+  
     
-    
-    #Dotal Puro (Pedro)
-    selectInput("seg", "Selecione o seguro:",choices = c("Seguro Temporário" = 1 ,"Seguro Vitalício" = 2,"Anuidade" = 3, "Dotal Puro" = 4, "Dotal Misto" = 5),multiple = F),
-    selectInput("tab", "Selecione a tábua de vida", choices = c("AT 49" = 1, "AT 83" = 2, "AT 2000" = 3)),
-    #checkboxInput("care", "Carencia", value = FALSE),
-    #conditionalPanel(condition = "input.care == TRUE", numericInput("TempCar", "Tempo de Carencia", value = 1)),
-    # Se a tábua at2000 for selecionada então o individuo pode escolher o sexo do participante.
-    conditionalPanel(condition = "input.tab == 3", selectInput("sex", "Sexo:",choices = c("Masculino" = 1 ,"Feminino" = 2), multiple = F)),
-    numericInput("ben", "Beneficio ($)", min = 0, max = Inf, value = 1),
-    numericInput("idade", "Idade", min = 0, max = (nrow(dados)-1), value = 0, step = 1),
-    
-    # Se o seguro de vida vitalício não for selecionado então o agoritmo não mostra o painel período.
-    conditionalPanel(condition = "input.seg != 2", numericInput("n", "Período",min = 0, max = (nrow(dados)-1), value = 1, step = 1)),
-    numericInput("tx", "Taxa de juros", min = 0, max = 1, value = 0.06, step = 0.001 ),
-    
-    
-    #Se a Seguro vitalício for selecionada...
-    conditionalPanel(condition = "input.dist==1",numericInput("Período","Taxa de juros",value = 0),
-                     numericInput("sd","Idade",value = 1,min = 0)),
-    
-    #Se a Uniforme for selecionada...
-    conditionalPanel(condition = "input.dist==2",numericInput("a","a",value = 0),
-                     numericInput("b","b",value = 1)),
-    
-    #Se a Exponencial for selecionada...
-    conditionalPanel(condition = "input.dist==3",numericInput("lambda","Lambda",value = 0.5,min=0))
-    
-    
-    
-    
-  ),dashboardBody(
-    
-    verbatimTextOutput("hist"),
-    plotOutput("gratabua"))
+    conditionalPanel(condition = "input.abaselecionada==1", 
+                     selectInput("seg", "Selecione o seguro:",choices = c("Seguro Temporário" = 1 ,"Seguro Vitalício" = 2) ,multiple = F),
+                     # Se a tábua at2000 for selecionada então o individuo pode escolher o sexo do participante.
+                     selectInput("tab", "Selecione a tábua de vida", choices = c("AT 49" = 1, "AT 83" = 2, "AT 2000" = 3)),
+                     conditionalPanel(condition = "input.tab == 3", selectInput("sex", "Sexo:",choices = c("Masculino" = 1 ,"Feminino" = 2), multiple = F)),
+                     numericInput("idade", "Idade", min = 0, max = (nrow(dados)-1), value = 0, step = 1),
+                     numericInput("ben", "Beneficio ($)", min = 0, max = Inf, value = 1),
+                     conditionalPanel(condition = "input.seg != 2", numericInput("n", "Período", min = 0, max = (nrow(dados)-1), value = 1, step = 1)),
+                     numericInput("tx", "Taxa de juros", min = 0, max = 1, value = 0.06, step = 0.001 )),
+    conditionalPanel(condition = "input.abaselecionada==2", 
+                     selectInput("anu", "Selecione o Produto:",choices = c("Anuidade" = 1, "Dotal Puro" = 2, "Dotal Misto" = 3) ,multiple = F),
+                     # Se a tábua at2000 for selecionada então o individuo pode escolher o sexo do participante.
+                     selectInput("tab", "Selecione a tábua de vida", choices = c("AT 49" = 1, "AT 83" = 2, "AT 2000" = 3)),
+                     conditionalPanel(condition = "input.tab == 3", selectInput("sex", "Sexo:", choices = c("Masculino" = 1 ,"Feminino" = 2), multiple = F)),
+                     numericInput("idade", "Idade", min = 0, max = (nrow(dados)-1), value = 0, step = 1),
+                     numericInput("ben", "Beneficio ($)", min = 0, max = Inf, value = 1),
+                     numericInput("tx", "Taxa de juros", min = 0, max = 1, value = 0.06, step = 0.001 ))
+  ),
+  dashboardBody(
+    tabsetPanel(type = "tab",
+                tabPanel("Seguros",column(width = 6,verbatimTextOutput("segs")),value = 1),
+                #tabPanel("Seguro Vitalício",verbatimTextOutput("vit"),value = 2),
+                tabPanel("Anuidade e Dotal",verbatimTextOutput("anuids"), value = 2),
+                #tabPanel("Dotal Puro",verbatimTextOutput("dotp"),value = 4),
+                #tabPanel("Dotal Misto",tableOutput("dotm"),value = 5)
+                id = "abaselecionada"),
+    plotOutput("gratabua")
+  )
 )
-
 
 # Funções -----------------------------------------------------------------
 #dados <- read.table('/home/walef/Dropbox/SECA Programação/tábuas_leitura_R.txt', h=T)
 #dados <- read.table('C:/Users/Yagho Note/interface-atuarial/tábuas_leitura_R.txt', h=T)
+
 attach(dados)
 SV_Temp <- function( i, idade, n, b, qx){ # i = taxa de juros, n = tempo, b = valor do beneficio
   px <- 1-qx
@@ -79,7 +74,6 @@ SV_Vit <- function( i, idade, b, qx){ # i = taxa de juros, n = tempo, b = valor 
   Ax <- round(Ax, 2)
   return (Ax)
 }
-
 
 # Verificar o cáculo Pedro
 Anuid = function( i, idade, n , b, qx){ # i= taxa de juros, n= período, b = benefício
@@ -109,8 +103,8 @@ Dotal<-function(i, idade, n, b, qx){
 # Server ------------------------------------------------------------------
 
 server <- function(input, output) {
-  
-  output$hist <- renderPrint({
+
+  output$segs <- renderPrint({
     if((max(dados$Idade)-input$idade) >= input$n){
       if(input$tab==1){
         qx <- dados$AT_49_qx
@@ -126,37 +120,50 @@ server <- function(input, output) {
           qx <- dados$AT_2000B_F_qx
         }
       }
-      
-      #if(input$care == "FALSE"){
-      #   care = 1
-      #   m = 0
-      # }else{
-      #   care = Dotal_Puro(input$tx, input$idade,input$TempCar, 1 , qx)
-      #   m = input$TempCar
-      # }
-      
       if(input$seg==1){
         a <- SV_Temp(input$tx, round(input$idade, 0), input$n, input$ben, qx)
       }
       if(input$seg==2){
         a <- SV_Vit(input$tx, input$idade, input$ben, qx)
-        }
-      if(input$seg==3){
-        a <- Anuid(input$tx, input$idade, input$n,  input$ben, qx)
-      }
-      if(input$seg==4){
-        a <- Dotal_Puro(input$tx, input$idade , input$n, input$ben, qx)
-      }
-      if(input$seg==5){
-        a <- Dotal(input$tx, input$idade, input$n, input$ben, qx)
       }
       cat('O prêmio puro único é:', a)
     }else{
       cat('O período temporário está errado')
     }
-    # print(local)
   })
   
+  output$anuids = renderPlot({
+    if((max(dados$Idade)-input$idade) >= input$n){
+      if(input$tab==1){
+        qx <- dados$AT_49_qx
+      }
+      if(input$tab==2){
+        qx <- dados$AT_83_qx
+      }
+      if(input$tab==3){
+        if(input$sex==1){
+          qx <- dados$AT_2000B_M_qx
+        }
+        if(input$sex==2){
+          qx <- dados$AT_2000B_F_qx
+        }
+      }
+    if(input$anu==1){
+      a <- Anuid(input$tx, input$idade, input$n,  input$ben, qx)
+    }
+    if(input$anu==2){
+      a <- Dotal_Puro(input$tx, input$idade , input$n, input$ben, qx)
+    }
+    if(input$anu==3){
+      a <- Dotal(input$tx, input$idade, input$n, input$ben, qx)
+    }
+    cat('O prêmio puro único é:', a)
+  }else{
+    cat('O período temporário está errado')
+  }
+})
+  
+
   output$gratabua = renderPlot({
     if((max(dados$Idade)-input$idade) >= input$n){
       if(input$tab==1){
@@ -176,20 +183,6 @@ server <- function(input, output) {
     }
     plot(10000*cumprod(1 -qx), type="l", color="green", width="20", xlab="Anos", ylab="População")
   })
-  
-  # output$grafico <- renderPlot({
-  #   if(input$seg==1){
-  #     a <- SV_Temp(input$tx, input$idade, input$n, input$ben)
-  #   }
-  #   if(input$seg==2){
-  #     a <- SV_Temp(input$tx, input$idade, input$n, input$ben)}
-  #   if(input$seg==3){
-  #     a <- SV_Temp(input$tx, input$idade, input$n, input$ben)
-  #   }
-  #   a
-  # })
-  
-  
 }
 
 shinyApp(ui, server)
