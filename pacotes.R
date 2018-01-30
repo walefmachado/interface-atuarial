7b70aca8231d1092ad72ef2c162aff922c05f4ec
yagholc <yagholc@hotmail.com>
2018-01-30 11:03:13 -0200

    Arquivo dos pacotes necessarios

---
+++ pacotes.R
@@ -0,0 +1,5 @@
+if(!require(shiny)){install.packages('shiny')}                      #Shiny
+if(!require(shinydashboard)){install.packages('shinydashboard')}    #Shiny Dashboard
+if(!require(ggplot2)){install.packages('ggplot2')}                  # Gráficos
+if(!require(corrplot)){install.packages('corrplot')}                # Representação de matrizes de correlações
+if(!require(plotly)){install.packages('plotly')}
