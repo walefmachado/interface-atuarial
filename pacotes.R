30263ab5334b638801df233bb02b2d0408f88094
Walef Machado <walefm2@hotmail.com>
2018-01-31 23:27:22 -0200

    comentários e pacotes

--- pacotes.R
+++ pacotes.R
@@ -1,5 +1,4 @@
 if(!require(shiny)){install.packages('shiny')}                      #Shiny
 if(!require(shinydashboard)){install.packages('shinydashboard')}    #Shiny Dashboard
 if(!require(ggplot2)){install.packages('ggplot2')}                  # Gráficos
-if(!require(corrplot)){install.packages('corrplot')}                # Representação de matrizes de correlações
-if(!require(plotly)){install.packages('plotly')}
+if(!require(plotly)){install.packages('plotly')}                    # Gráficos interativos 
